48dcc9a038ab6b89d22f9d48fb2ee1ecc0be98a8
Ananyakg1 <147647308+Ananyakg1@users.noreply.github.com>
2025-02-10 09:13:05 +0530

    Create terraform.tf

---
+++ prod/terraform.tf
@@ -0,0 +1,5 @@
+subscription_id = "846df436-d658-402a-b0dd-89022775043b"
+resource_group_name  = "prodresourcegroupbit"
+location            = "East US"
+storage_account_name = "prodstorageaccountbit"
+container_name       = "prodcontainerbit"
